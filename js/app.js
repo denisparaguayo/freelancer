592b8b38b758cddec2760c7fcded03ede07ea393
Denis Ricardo <denis.cantero1987@gmail.com>
2021-02-17 01:59:45 -0300

    cambio en footer y tipografia

--- js/app.js
+++ js/app.js
@@ -1,14 +1,5 @@
-    // Add A Dynamic Year In Divi Footer Copyright
-    var beg_year = 2017; // Year site went live
-    var this_year = new Date(); // Get current date
-    var copyright_text = ' Nombre Empresa | Derechos Reservados ';
-    var developed_by = '| Diseñado con <span id="icon-love" class="et-pb-icon et-waypoint et_pb_animation_top et-animated"></span> por <a href="https://webpy.xyz/" target="_blank">Denis Cantero</a>'; // Developer info
-    this_year = this_year.getFullYear();
-    if (this_year > beg_year) {
-        var year = beg_year + ' - ' + this_year;
-    } else {
-        var year = this_year;
-    }
-    jQuery(($) => {
-        $('#footer-info').html(`© ${year} ${copyright_text} ${developed_by}`);
-    });
+var year = new Date().getFullYear();
+
+var date = `&copy;  2019 -  ${year}. Todos los Derechos Reservados.`;
+
+document.getElementsByClassName('año')[0].innerHTML = date;
